2a76213dd9d836f656bbf2c72f4bd396193d0462
trinhdtn <trinhdtn@dipro.vn>
2023-01-04 16:59:48 +0700

    goi bai tap 1

---
+++ file.js
@@ -0,0 +1,14 @@
+console.log("Happy new year!");
+console.log("Happy new year!");
+console.log("Happy new year!");
+console.log("Happy new year!");
+console.log("Happy new year!");
+
+const num = 1;
+if (num %2 ==0) {
+
+    console.log("num is odd");
+} else {
+    console.log("num is even");
+}
+           
