406feeef487a4c325661e756aae91510395bc175
Le Hoang Huy <lehoanghuyit1009@gmil.com>
2023-01-05 10:00:13 +0700

    Yennhi_baitap1

---
+++ baitap1.js
@@ -0,0 +1,14 @@
+// bai tap 1
+console.log("Happy new year!");
+console.log("Happy new year!");
+console.log("Happy new year!");
+console.log("Happy new year!");
+console.log("Happy new year!");
+
+// bai tap 2
+const num = 4;
+if (num % 2 == 0) {
+  console.log(`${num} la so chan`);
+} else {
+  console.log(`${num} la so le`);
+}
