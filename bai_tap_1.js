8dc233bd52b52e88cbba588b7235377a1694d056
dipro <>
2023-01-06 11:39:43 +0700

    hoan thanh bai tap

---
+++ bai_tap_1.js
@@ -0,0 +1,14 @@
+//bai_tap_1//
+console.log("Happy new year!");
+console.log("Happy new year!");
+console.log("Happy new year!");
+console.log("Happy new year!");
+console.log("Happy new year!")
+
+//bai_tap_2
+const num = 7
+if (num % 2 ==0){
+    console.log(`${num}la so chan`);
+} else{
+    console.log(`${num}la so le`)
+}
