1954168a86bbac9ed796f5c97338d42568650e4e
Mac <huongntt@gmail.com>
2023-01-08 00:28:05 +0700

    Hoan thanh bai tap 1

---
+++ HuongNTT_Baitap1.js
@@ -0,0 +1,17 @@
+console.log ("Happy new year!")
+console.log ("Happy new year!")
+console.log ("Happy new year!")
+console.log ("Happy new year!")
+console.log ("Happy new year!")
+//In ra màn hình 5 lần dòng chữ "Happy new year!"
+
+//const number = prompt("Nhập số cần check: ");
+//Vì sao nếu dùng cách trên lại báo lỗi "Prompt is not define"?
+const number=6;
+if(number % 2 == 0) {
+    console.log("Số này là số chẵn.");
+}
+else {
+    console.log("Số này là số lẻ.");
+}
+//Cho 1 số bất kỳ và in ra màn hình số vừa cho là chẵn hay lẻ
