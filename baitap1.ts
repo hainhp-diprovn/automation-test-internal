ea8660aeac17397a8d7fcaa4654a29e5f39ce6e8
hainhp-diprovn <hainhp@dipro.vn>
2023-02-08 08:58:14 +0700

    Template project

---
+++ baitap1.ts
@@ -0,0 +1,41 @@
+
+
+
+
+// ĐTB >= 8 : xếp loại GIỎI
+// 8 > ĐTB >= 6.5 : xếp loại KHÁ
+// 6.5 > ĐTB >= 5 : xếp loại TRUNG BÌNH
+// 5 > ĐTB >= 3.5 : xếp loại YẾU
+// 3.5 > ĐTB : xếp loại KÉM
+
+const diem: number = 3;
+
+if (diem >= 8) {
+    console.log("Gioi");
+} else if (diem > 6.5) {
+    console.log("Kha");
+} else if (diem >= 5) {
+    console.log("Trung binh");
+} else if (diem > 3.5) {
+    console.log("Yeu");
+} else {
+    console.log("Kem");
+}
+
+
+// console.log("Hello world");
+// console.log("Hello world");
+// console.log("Hello world");
+// console.log("Hello world");
+// console.log("Hello world");
+
+const wish = "Hello world";
+
+for (var hai = 1; hai <= 10; hai++) {
+    console.log(hai);
+    // console.log(wish);
+}
+
+console.log("End loop");
+
+
