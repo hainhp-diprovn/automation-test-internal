75ee1d67a9cd34ced75db37d17ba3e6a67bb2f2c
dipro <>
2023-02-13 13:55:25 +0700

    hoan thanh bai tap 2

---
+++ baitap2_bien_huyen.js
@@ -0,0 +1,16 @@
+// Bai tap co su dung Bien 
+// Bai tap 1
+console.log("Tôi năm nay 18 tuổi \nTôi có hoài bão để theo đuổi")
+
+// Bai Tap 2
+const sn1 = 3, sn2 = 4, sn3 = 2.5;
+console.log(sn1, sn2, sn3)
+
+
+//Bai tap 3
+const sn4 = 2, 
+sn5 = 3, 
+sn6 = 4;
+if (sn4 % 2 == 0) console.log(sn4);
+if (sn5 % 2 == 0) console.log(sn5);
+if (sn6 % 2 == 0) console.log(sn6)
